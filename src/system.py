84d3d26f455d6d8420f5e80a1cf7b227b28bb6b5
Augustin <augustinkoyo1997@gmail.com>
2026-08-19 00:37:34 +0100

    Add system infomation collector

---
+++ src/system.py
@@ -0,0 +1,12 @@
+import platform
+import socket
+
+def get_system_info():
+    """Return basic information about the current system"""
+    return {
+        "hostname":socket.gethostname(),
+        "operating_system":platform.system(),
+        "os_release":platform.release(),
+        "architecture":platform.machine(),
+        "python_version":platform.python_version()
+    }
