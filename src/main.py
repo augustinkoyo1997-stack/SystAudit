9753f17a07b866814a9be701597cf87b336fee4a
Augustin <augustinkoyo1997@gmail.com>
2026-08-21 22:44:09 +0100

    Add system information entry point

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 print("SystemAudit - system information Tool")
 #version  synchronised whith Github
+
+from system import get_system_info
+def main():
+   system_info=get_system_info()
+   print("==SysAudit - System Information ==")
+   for key,value in system_info.items():
+     print(f"{key}: {value}")
+if __name__=="__main__":
+   main()
